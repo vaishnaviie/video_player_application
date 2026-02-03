f5c346197fad5f0aa4a035712bcf38bc983daf68
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2026-02-04 02:20:00 +0530

    rendered videos over UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-background h-screen">
+    <div className="bg-background ">
       <Home />
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { categories } from "../data/data.json";
 
 const Home = () => {
+  const [data, setdata] = useState(categories[0].contents);
   return (
     <div>
       <ul className=" flex justify-center gap-4">
         {categories.map((category) => (
-          <li className="font-roboto text-sm border border-accent p-1 rounded-sm bg-card text-text-white ">
+          <li
+            key={category.category.slug}
+            className="font-roboto text-sm border border-accent p-1 rounded-sm bg-card text-text-white font-semibold "
+          >
             {category?.category?.name}
           </li>
         ))}
       </ul>
+
+      <ol className=" flex flex-col  ">
+        {data.map((category) => (
+          <li className="mb-3">
+            <iframe
+              width="100%"
+              height="200px"
+              className=" object-contain"
+              src={category?.mediaUrl}
+            ></iframe>
+
+            <div className=" flex gap-2 items-center p-2">
+              <img
+                className=" h-8 w-8 rounded-full object-cover"
+                src={category.thumbnailUrl}
+                alt={category.thumbnailUrl}
+              />
+              <p className=" text-text-white ">{category.title}</p>
+            </div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
