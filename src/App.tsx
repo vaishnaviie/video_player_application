22f782f06ca748ddefa5dc8644e60b41d951c9e4
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2026-02-04 08:26:59 +0530

    added routes, responsiveness and autoplay functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VideoPlayer from "./pages/VideoPlayer";
 
 function App() {
   return (
-    <div className="bg-background box-border">
-      <Home />
-    </div>
+    <Router>
+      <div className="bg-background box-border min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/video/:videoId" element={<VideoPlayer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { categories } from "../data/data.json";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [data, setdata] = useState(categories[0].contents);
+  const navigate = useNavigate();
+
   return (
     <div className="relative">
       <ul className="fixed flex justify-center gap-4 bg-background pb-2 w-full">
@@ -18,11 +21,13 @@ const Home = () => {
 
       <ol className=" flex flex-col md:grid md:grid-cols-2 md:gap-4 xl:grid-cols-3 2xl:grid-cols-4 pt-12   ">
         {data.map((category) => (
-          <div className="mb-3">
-            <iframe
-              className=" w-[90%] h-[150px] md:h-[200px] lg:h-[250px] mx-4  rounded-md"
-              src={category?.mediaUrl}
-            ></iframe>
+          <div className="mb-3" key={category?.slug}>
+            <img
+              className=" w-[90%] h-[150px] md:h-[200px] lg:h-[250px] mx-4  rounded-md object-cover object-[50%_20%]"
+              src={category?.thumbnailUrl}
+              alt={category?.thumbnailUrl}
+              onClick={() => navigate(`/video/${category?.slug}`)}
+            />
 
             <div className="flex gap-2 items-center p-2">
               <img
